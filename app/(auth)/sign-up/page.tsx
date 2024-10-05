774265b87227bbe08fe3fe7be4ae49fcda310848
khwahish vaid <kvaid1@myseneca.ca>
2024-10-04 22:25:07 -0400

    AuthPage (Commit:1)

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,13 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignUp = () => {
+const Signup = () => {
     return (
-    <div>SignUp</div>
+        <section className='flex-center size-full max-sm:px-6'>
+            <AuthForm type='sign-up'/>
+        </section>
     )
 }
 
 
-export default SignUp
+export default Signup
